d1f319caa6c3829eff847da20ef4cdd6db430b46
Neharika-Dasappagari <neharikadasappagari@gmail.com>
2025-07-03 17:36:01 +0530

    average

---
+++ Average/task_4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int num_days;
+	int num_years=num_days/365;
+	int num_months=num_days%365/30;
+	int num_weeks=num_days%365%30/7;
+	int num_remaining_days=num_days%365%30%7;
+	printf("enter the num_days:");
+	scanf("%d",&num_days);
+	printf("\n the num_years:%d",num_days/365);
+	printf("\n the num_months:%d",num_days%365/30);
+	printf("\n the num_weeks:%d",num_days%365%30/7);
+	printf("\n the num_remaining_days:%d",num_days%365%30%7);
+	return 1;
+}
